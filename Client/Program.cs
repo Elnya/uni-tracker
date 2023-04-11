8022e07b3d6ed43a7d9d0e551dcbd280613ed5c3
Elnya <karoness@outlook.com>
2023-04-11 09:48:24 +0900

    Add: FluentUI library and a test slider

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+// To use FluentUI library
+using Microsoft.Fast.Components.FluentUI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+// Add FluentUI components
+LibraryConfiguration config = new(ConfigurationGenerator.GetIconConfiguration(), ConfigurationGenerator.GetEmojiConfiguration());
+builder.Services.AddFluentUIComponents(config);
 
 await builder.Build().RunAsync();
